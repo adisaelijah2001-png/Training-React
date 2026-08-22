fd633e6b812c8bbd2582adf1da260d2b8945648c
Adisa Elijah Abisola <adisaelijah2001@gmail.com>
2026-08-22 16:38:35 +0100

    i import bloglist

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Home from './assets/Home'
+import Bloglist from './assets/Bloglist'
 
 function App() {
   return(
     <div className="app-shell">
       <Home />
+      <div className="blo-list">
+        <Bloglist />
+      </div>
     </div>
   );
 }
